ccbd6f5ef8b9ce71ffa8a9b050a0b2ea36863be0
Sam P <33042752+SamP923@users.noreply.github.com>
2020-03-08 20:07:09 -0700

    maybe fix

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneTestAuto.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="Skystone Autonomous")
+public class SkystoneTestAuto extends LinearOpMode {
+    @Override
+    public void runOpMode(){
+
+    }
+}
